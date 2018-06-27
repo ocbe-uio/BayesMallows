06ddfafa7bed5d50338f234ef3ffb7389735f9ec
Øystein Sørensen <oystein_sorensen@hotmail.com>
2018-06-27 05:33:44 +0200

    Added potato data

---
+++ R/data.R
@@ -0,0 +1,22 @@
+#' True ranking of the weights of 20 potatoes.
+#'
+#'
+#' @format An integer vector with 20 elements
+#' @references Insert reference to review paper where the potato data is presented.
+"potato_true_ranking"
+
+#' Result of ranking potatoes by weight, where the assessors were only allowed
+#' to inspected the potatoes visually. 12 assessors ranked 20 potatoes.
+#'
+#'
+#' @format An integer matrix with 12 rows and 20 columns.
+#' @references Insert reference to review paper where the potato data is
+#'   presented.
+"potato_visual"
+
+#' Result of ranking potatoes by weight, where the assessors were allowed to lift the potatoes. 12 assessors ranked 20 potatoes.
+#'
+#'
+#' @format An integer matrix with 12 rows and 20 columns.
+#' @references Insert reference to review paper where the potato data is presented.
+"potato_visual"
