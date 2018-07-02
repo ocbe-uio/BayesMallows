0d431a946d0d3ee6cdc06079780371d42bca3651
Øystein Sørensen <oystein_sorensen@hotmail.com>
2018-07-02 20:37:09 +0200

    Added test of mcmc

---
+++ tests/testthat/test_mcmc_function.R
@@ -0,0 +1,24 @@
+context("Testing MCMC function on potato data")
+
+# Testing with footrule
+model_fit <- compute_mallows(potato_weighing, "footrule", nmc = 10000, burnin = 5000)
+test_that(
+  "alpha is in a decent range for footrule",
+  expect_true(
+    model_fit$alpha > 10 && model_fit$alpha < 20
+  )
+)
+
+test_that(
+  "acceptance rate is acceptable for footrule",
+  expect_true(
+    mean(model_fit$alpha_acceptance) > 0 && mean(model_fit$alpha_acceptance < 1)
+  )
+)
+
+test_that(
+  "acceptance rate is acceptable for rho",
+  expect_true(
+    mean(model_fit$rho_acceptance) > 0 && mean(model_fit$rho_acceptance) < 1
+  )
+)
