a32898b1f037d73644d97557f957ac6324f377e0
Øystein Sørensen <oystein_sorensen@hotmail.com>
2018-08-06 07:52:26 +0200

    Documented function for converting between orderings and ranks. Closes #4

--- R/rank_conversion.R
+++ R/rank_conversion.R
@@ -1,8 +1,15 @@
 #' Convert between ranking and ordering.
 #'
-#' @param O A vector or matrix of ordered items. If a matrix,
-#' it should be of size N times n, where N is the number of samples
-#' and n is the number of items.
+#' \code{create_ranking} takes a vector or matrix of ordered items \code{O} and
+#' returns a corresponding vector or matrix of ranked items.
+#' \code{create_ordering} takes a vector or matrix of rankings \code{R} and
+#' returns a corresponding vector or matrix of ordered items.
+#'
+#' @param O A vector or matrix of ordered items. If a matrix, it should be of
+#'   size N times n, where N is the number of samples and n is the number of
+#'   items.
+#' @param R A vector or matrix of ranked items. If a matrix, it should be N
+#'   times n, where N is the number of samples and n is the number of items.
 #'
 #' @return A vector or matrix of rankings.
 #' @export
@@ -14,6 +21,10 @@
 #' # Get ranks
 #' R <- create_ranking(O)
 #' # R is c(2, 3, 5, 4, 1)
+#' # Finally we convert it backed to an ordering.
+#' O2 <- create_ordering(R)
+#' # Confirm that we get back what we had
+#' all.equal(O, O2)
 #'
 #' # Next, we have a matrix with N = 19 samples
 #' # and n = 4 items
@@ -23,6 +34,10 @@
 #' O <- t(replicate(N, sample.int(n)))
 #' # Convert the ordering to ranking
 #' R <- create_ranking(O)
+#' # Now we try to convert it back to an ordering.
+#' O2 <- create_ordering(R)
+#' # Confirm that we get back what we had
+#' all.equal(O, O2)
 #'
 create_ranking <- function(O){
 
@@ -37,4 +52,10 @@ create_ranking <- function(O){
   } else {
     stop("O must be a vector or matrix")
   }
+}
+
+#' @describeIn create_ranking Convert from ranking to ordering.
+#' @export
+create_ordering <- function(R){
+  create_ranking(R)
 }
