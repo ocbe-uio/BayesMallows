9294cdc880c9c1472dd876b3bb4f2402b4bb0718
Øystein Sørensen <oystein_sorensen@hotmail.com>
2018-08-27 17:07:08 +0200

    Added function to header

--- src/partitionfuns.h
+++ src/partitionfuns.h
@@ -5,6 +5,10 @@
 #include "misc.h"
 #include "distfuns.h"
 
-double get_partition_function(int, double, Rcpp::Nullable<arma::vec>, Rcpp::Nullable<arma::vec>, std::string);
+double get_partition_function(int, double, Rcpp::Nullable<arma::vec>,
+                              Rcpp::Nullable<arma::vec>, std::string);
+
+arma::vec asymptotic_partition_function(arma::vec alpha_grid, std::string metric,
+                                        int K, int n_iterations, int n_items);
 
 #endif
