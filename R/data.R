abe6131eb430e3143da7068450497d70ae60d117
Øystein Sørensen <oyss@medisin.uio.no>
2018-08-31 13:51:03 +0200

    Added references for potato data

--- R/data.R
+++ R/data.R
@@ -1,26 +1,19 @@
 #' True ranking of the weights of 20 potatoes.
 #'
-#'
-#' @format An integer vector with 20 elements
-#' @references Insert reference to review paper where the potato data is presented.
+#' @references \insertRef{liu2018}{BayesMallows}
 "potato_true_ranking"
 
 #' Result of ranking potatoes by weight, where the assessors were only allowed
 #' to inspected the potatoes visually. 12 assessors ranked 20 potatoes.
 #'
-#'
-#' @format An integer matrix with 12 rows and 20 columns.
-#' @references Insert reference to review paper where the potato data is
-#'   presented.
+#' @references \insertRef{liu2018}{BayesMallows}
 "potato_visual"
 
-#' Result of ranking potatoes by weight, where the assessors were allowed to lift the potatoes. 12 assessors ranked 20 potatoes.
+#' Result of ranking potatoes by weight, where the assessors were
+#' allowed to lift the potatoes. 12 assessors ranked 20 potatoes.
 #'
-#'
-#' @format An integer matrix with 12 rows and 20 columns.
-#' @references Insert reference to review paper where the potato data is presented.
+#' @references \insertRef{liu2018}{BayesMallows}
 "potato_weighing"
-
 
 #' Beach Preferences
 #'
