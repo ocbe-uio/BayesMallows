6b9c20a84a9e4705d415ad5d49aa87c49295b746
Øystein Sørensen <oyss@medisin.uio.no>
2018-08-31 14:59:22 +0200

    More documentation

---
+++ R/compute_map_consensus.R
@@ -0,0 +1,50 @@
+#' Maximum a Posterior Consensus Ranking
+#'
+#' Compute the maximum a posterior consensus ranking. If a mixture model
+#' is supplied, the maximum a posterior consensus ranking for each mixture
+#' is returned.
+#'
+#' @param model_fit An object of type \code{BayesMallows}, returned from
+#'   \code{\link{compute_mallows}}.
+#'
+#' @param burnin A numeric value specifying the number of iterations
+#' to discard as burn-in. See \code{\link{assess_convergence}}.
+#'
+#' @export
+#'
+#' @example /inst/examples/compute_map_consensus_example.R
+#'
+compute_map_consensus <- function(model_fit, burnin){
+  df <- dplyr::filter(model_fit$rho, .data$iteration > burnin)
+
+  # Store the total number of iterations after burnin
+  n_samples <- length(unique(df$iteration))
+
+  # Spread to get items along columns
+  df <- tidyr::spread(df, key = .data$item, value = .data$value)
+
+  # Group by everything except iteration, and count the unique combinations
+  df <- dplyr::group_by_at(df, .vars = dplyr::vars(-.data$iteration))
+  df <- dplyr::count(df)
+  df <- dplyr::ungroup(df)
+  # Keep only the maximum per cluster
+  df <- dplyr::group_by(df, .data$cluster)
+  df <- dplyr::mutate(df, n_max = max(.data$n))
+  df <- dplyr::filter(df, .data$n == .data$n_max)
+  df <- dplyr::ungroup(df)
+
+  # Compute the probability
+  df <- dplyr::mutate(df, probability = .data$n / n_samples)
+  df <- dplyr::select(df, -.data$n_max, -.data$n)
+
+  # Now collect one set of ranks per cluster
+  df <- tidyr::gather(df, key = "item", value = "map_ranking",
+                -.data$cluster, -.data$probability)
+
+  if(model_fit$n_clusters >= 1){
+    df <- dplyr::select(df, -.data$cluster)
+  }
+
+  return(df)
+
+}
--- inst/examples/compute_map_consensus_example.R
+++ inst/examples/compute_map_consensus_example.R
@@ -6,24 +6,33 @@ model_fit <- compute_mallows(potato_visual)
 # Se the documentation to compute_mallows for how to assess the convergence of the algorithm
 # Having chosen burin = 1000, we compute posterior intervals
 burnin <- 1000
-# We then compute the CP consensus.
-compute_cp_consensus(model_fit, burnin = burnin)
+# We then compute the MAP consensus.
+compute_map_consensus(model_fit, burnin = burnin)
 
 \dontrun{
-  # CLUSTERWISE CP CONSENSUS
+  # CLUSTERWISE MAP CONSENSUS
   # We can run a mixture of Mallows models, using the n_clusters argument
   # We use the sushi example data. See the documentation of compute_mallows for a more elaborate
   # example
   model_fit <- compute_mallows(sushi_rankings, n_clusters = 5)
-  # Keeping the burnin at 1000, we can compute the CP consensus per cluster
-  cp_consensus_df <- compute_cp_consensus(model_fit, burnin = burnin)
+  # Keeping the burnin at 1000, we can compute the MAP consensus per cluster
+  map_consensus_df <- compute_map_consensus(model_fit, burnin = burnin)
   # Using dplyr::select and tidyr::cumprob we can now make a table
   # which shows the ranking in each cluster:
   library(dplyr)
   library(tidyr)
-  cp_consensus_df %>%
-    select(-cumprob) %>%
+  map_consensus_df %>%
+    select(-probability) %>%
     spread(key = cluster, value = item)
 }
 
+\dontrun{
+  # MAP CONSENSUS FOR PAIRWISE PREFENCE DATA
+  # We use the example dataset with beach preferences.
+  model_fit <- compute_mallows(preferences = beach_preferences)
+  # We set burnin = 1000
+  burnin <- 1000
+  # We now compute the MAP consensus
+  map_consensus_df <- compute_map_consensus(model_fit, burnin = burnin)
+}
 
