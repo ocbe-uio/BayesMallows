4e2a2f9baac4bb013b6c0617075f9cf1bc071410
Øystein Sørensen <oyss@medisin.uio.no>
2018-10-16 10:20:56 +0200

    Updated slurm script, ready for proper importance sampling

--- data-raw/importance_sampling/estimate_z.R
+++ data-raw/importance_sampling/estimate_z.R
@@ -1,13 +1,13 @@
 # Then we do importance sampling for Spearman distance
 library(parallel)
-n_items <- seq(from = 15L, to = 22L, by = 1L)
+n_items <- seq(from = 15L, to = 30L, by = 1L)
 num_workers <- as.integer(Sys.getenv("SLURM_NTASKS")) - 1
 myCluster <- makeCluster(num_workers)
 
 system.time(estimates <- parLapply(myCluster, n_items, function(n_items) {
   BayesMallows::estimate_partition_function(method = "importance_sampling",
                                             alpha_vector = seq(from = 0, to = 20, by = 0.1),
-                                            n_items = n_items, metric = "spearman", nmc = 1e3,
+                                            n_items = n_items, metric = "spearman", nmc = 1e6,
                                             degree = 9)
 }))
 
