0874205791dd445f184c1d8f26f9b5196ffa16dc
Øystein Sørensen <oyss@medisin.uio.no>
2018-10-16 09:44:04 +0200

    Updated parallel computation script

--- data-raw/importance_sampling/estimate_z.R
+++ data-raw/importance_sampling/estimate_z.R
@@ -1,12 +1,28 @@
 # Then we do importance sampling for Spearman distance
-library(doParallel)
-cl <- makeCluster(2)
-registerDoParallel(cl)
-system.time({estimates <- foreach(it = 15:22) %dopar% {
-  BayesMallows::estimate_partition_function(alpha_vector = seq(from = 0, to = 20, by = .1),
-                                            n_items = it, metric = "spearman", nmc = 1e6,
+library(parallel)
+n_items <- seq(from = 15L, to = 22L, by = 1L)
+num_workers <- as.integer(Sys.getenv("SLURM_NTASKS")) - 1
+myCluster <- makeCluster(num_workers, type = "MPI")
+
+estimates <- parLapply(myCluster, n_items, function(n_items) {
+  BayesMallows::estimate_partition_function(method = "importance_sampling",
+                                            alpha_vector = seq(from = 0, to = 20, by = 0.1),
+                                            n_items = n_items, metric = "spearman", nmc = 1e3,
                                             degree = 9)
-}})
-stopCluster(cl)
+})
+
+stopCluster(myCluster)
+Rmpi::mpi.finalize()
+
+library(dplyr)
+estimates <- tibble(
+  n_items = n_items,
+  values = estimates
+) %>%
+  mutate(
+    metric = "spearman",
+    type = "importance_sampling",
+    message = "Partition function estimated with importance sampling for alpha between 0 and 20."
+  )
 
 save(estimates, file = "estimates.RData")
