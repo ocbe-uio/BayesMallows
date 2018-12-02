f38325e1fef2fae7730243bd5258cd36816f4200
Øystein Sørensen <oyss@medisin.uio.no>
2018-12-02 07:12:34 +0100

    Added integer sequence for Ulam distance up to 50 items

--- data-raw/generate_internal_data.R
+++ data-raw/generate_internal_data.R
@@ -31,11 +31,43 @@ partition_function_data <- tibble(
   )
 rm(seq2)
 
+# Then we compute the integer sequences for Ulam distance
+library(doParallel)
+cl <- makeCluster(8)
+registerDoParallel(cl)
+n_items <- 50
+
+seq_ulam <- foreach(ni = 1:n_items) %dopar% {
+  Z <- numeric(ni)
+  for(d in 0:(ni - 1)){
+    Z[d + 1] <- PerMallows::count.perms(perm.length = ni, dist.value = d, dist.name = "ulam")
+  }
+  Z
+}
+
+
+# We add these to the tibble
+partition_function_data <- tibble(
+  n_items = 1:n_items,
+  metric = rep("ulam", length(seq_ulam)),
+  values = seq_ulam
+) %>%
+  bind_rows(partition_function_data) %>%
+  mutate(
+    type = "cardinalities",
+    message = "Using exact partition function."
+  )
+rm(seq2)
+
+
 # Then we add importance sampling estimates
 load("./data-raw/importance_sampling/estimates.RData")
 
 partition_function_data <- partition_function_data %>%
   bind_rows(estimates)
+
+
+
 
 # Finally, keep the rows in pfd_old which are not in partition_function_data
 partition_function_data <- pfd_old %>%
@@ -43,4 +75,4 @@ partition_function_data <- pfd_old %>%
   bind_rows(partition_function_data)
 
 # Finally, save the fit as internal data
-devtools::use_data(partition_function_data, internal = TRUE, overwrite = TRUE)
+usethis::use_data(partition_function_data, internal = TRUE, overwrite = TRUE)
