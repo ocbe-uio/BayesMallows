e599c18bc124c04abb3893e5fc327345fbef825e
Øystein Sørensen <oyss@medisin.uio.no>
2019-01-05 12:51:18 +0100

    Restructured generate_constraints

--- R/generate_constraints.R
+++ R/generate_constraints.R
@@ -36,46 +36,49 @@
 generate_constraints <- function(preferences, n_items){
   # Turn the preferences dataframe into a list of dataframes,
   # one list element per assessor
-  constraints <- split(preferences, preferences$assessor)
+  constraints <- split(preferences[, c("bottom_item", "top_item"), drop = FALSE], preferences$assessor)
+  lapply(constraints, constraint_fun, n_items)
+}
 
-  purrr::map(constraints, function(x){
-    # Find out which items are constrained
-    constrained_items <- unique(c(x[["bottom_item"]], x[["top_item"]]))
 
-    # Now we must complete the dataframe with the items that do not appear
-    items_above <- tidyr::complete(dplyr::select(x, .data$bottom_item, .data$top_item),
-                                   bottom_item = seq(from = 1, to = n_items, by = 1),
-                                   fill = list(top_item = NA_integer_))
 
-    # Split it into a list, with one element per bottom_item
-    items_above <- split(items_above, items_above[["bottom_item"]])
+constraint_fun <- function(x, n_items){
+  # Find out which items are constrained
+  constrained_items <- unique(c(x[["bottom_item"]], x[["top_item"]]))
 
-    # For each item, find which items are ranked above it
-    items_above <- purrr::map(items_above, function(x) {
-      res <- unique(x[["top_item"]])
-      res <- res[!is.na(res)]
-    })
+  # Now we must complete the dataframe with the items that do not appear
+  items_above <- tidyr::complete(dplyr::select(x, .data$bottom_item, .data$top_item),
+                                 bottom_item = seq(from = 1, to = n_items, by = 1),
+                                 fill = list(top_item = NA_integer_))
 
-    # Now we must complete the dataframe with the items that do not appear
-    items_below <- tidyr::complete(dplyr::select(x, .data$bottom_item, .data$top_item),
-                                   top_item = seq(from = 1, to = n_items, by = 1),
-                                   fill = list(bottom_item = NA_integer_))
+  # Split it into a list, with one element per bottom_item
+  items_above <- split(items_above, items_above[["bottom_item"]])
 
-    # Split it into a list, with one element per bottom_item
-    items_below <- split(items_below, items_below[["top_item"]])
-
-    # For each item, find which items are ranked above it
-    items_below <- purrr::map(items_below, function(x) {
-      res <- unique(x[["bottom_item"]])
-      res <- res[!is.na(res)]
-    })
-
-    return(
-      list(
-        constrained_items = constrained_items,
-        items_above = items_above,
-        items_below = items_below
-      )
-    )
+  # For each item, find which items are ranked above it
+  items_above <- purrr::map(items_above, function(x) {
+    res <- unique(x[["top_item"]])
+    res <- res[!is.na(res)]
   })
+
+  # Now we must complete the dataframe with the items that do not appear
+  items_below <- tidyr::complete(dplyr::select(x, .data$bottom_item, .data$top_item),
+                                 top_item = seq(from = 1, to = n_items, by = 1),
+                                 fill = list(bottom_item = NA_integer_))
+
+  # Split it into a list, with one element per bottom_item
+  items_below <- split(items_below, items_below[["top_item"]])
+
+  # For each item, find which items are ranked above it
+  items_below <- purrr::map(items_below, function(x) {
+    res <- unique(x[["bottom_item"]])
+    res <- res[!is.na(res)]
+  })
+
+  return(
+    list(
+      constrained_items = constrained_items,
+      items_above = items_above,
+      items_below = items_below
+    )
+  )
 }
