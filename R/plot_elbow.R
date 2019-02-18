a2f1219c381bb60d669615b16bda8ae39fd2d734
Øystein Sørensen <oyss@medisin.uio.no>
2019-02-18 12:37:51 +0100

    Corrected y-axis label in elbow plot

--- R/plot_elbow.R
+++ R/plot_elbow.R
@@ -61,7 +61,7 @@ plot_elbow <- function(..., burnin = NULL){
   ggplot2::ggplot(df, ggplot2::aes(x = as.factor(.data$n_clusters), y = .data$value)) +
     ggplot2::geom_boxplot() +
     ggplot2::xlab("Number of clusters") +
-    ggplot2::ylab("Within-cluster som of distances") +
+    ggplot2::ylab("Within-cluster sum of distances") +
     ggplot2::ggtitle("Elbow Plot")
 
 
