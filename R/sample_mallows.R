1bf834b0b03b36020eb9d830eecd01968f3e05a0
Cristina Mollica <cmollica@MBP-di-Cristina.home>
2020-12-07 15:21:36 +0100

    Alignment of the default value of the leap_size argument to that of the compute_mallows

--- R/sample_mallows.R
+++ R/sample_mallows.R
@@ -42,7 +42,7 @@
 #' @example /inst/examples/sample_mallows_example.R
 #'
 sample_mallows <- function(rho0, alpha0, n_samples,
-                           leap_size = 1,
+                           leap_size = max(1L, floor(n_items/5)),
                            metric = "footrule",
                            diagnostic = FALSE,
                            burnin = ifelse(diagnostic, 0, 1000),
