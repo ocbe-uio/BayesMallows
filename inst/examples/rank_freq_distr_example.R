b9dd8ed23f7ceec6336e7703f90f3f72e4f64994
Øystein Sørensen <oyss@medisin.uio.no>
2021-01-06 14:43:15 +0100

    Removed which() which was strictly not necessary and added some documentation

--- inst/examples/rank_freq_distr_example.R
+++ inst/examples/rank_freq_distr_example.R
@@ -1,3 +1,6 @@
-data0 <- sample_mallows(rho0=1:5,alpha=10,n_samples=1000)
-
+# Create example data. We set the burn-in and thinning very low
+# for the sampling to go fast
+data0 <- sample_mallows(rho0 = 1:5, alpha=10, n_samples = 1000,
+                        burnin = 10, thinning = 1)
+# Find the frequency distribution
 rank_freq_distr(rankings=data0)
