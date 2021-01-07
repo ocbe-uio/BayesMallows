fda53b9ef9a665fc93fd59a92189ae33e32e612c
Øystein Sørensen <oyss@medisin.uio.no>
2021-01-07 20:34:19 +0100

    Added unit tests and formatted some of the documentation

--- R/Lik_DB_MIX.R
+++ R/Lik_DB_MIX.R
@@ -1,34 +1,43 @@
 #' Likelihood and log-likelihood evaluation for a Mallows mixture model
 #'
-#' @description Compute either the likelihood or the log-likelihood value of the Mallows mixture model parameters for a dataset of complete rankings.
-#' @param rho A matrix of size \code{n_clusters x n_items} whose rows are permutations of the first n_items integers corresponding to the modal rankings of the Mallows mixture components.
-#' @param alpha A vector of \code{n_clusters} non-negative scalar specifying the scale (precision) parameters of the Mallows mixture components.
-#' @param weights A vector of \code{n_clusters} non-negative scalars specifying the mixture weights.
-#' @param metric Character string specifying the distance measure to use. Available options are \code{"kendall"}, \code{"cayley"}, \code{"hamming"}, \code{"ulam"} for \code{n_items<=95}, \code{"footrule"} for \code{n_items<=50} and \code{"spearman"} for \code{n_items<=14}.
+#' @description Compute either the likelihood or the log-likelihood value of the
+#'   Mallows mixture model parameters for a dataset of complete rankings.
+#' @param rho A matrix of size \code{n_clusters x n_items} whose rows are
+#'   permutations of the first n_items integers corresponding to the modal
+#'   rankings of the Mallows mixture components.
+#' @param alpha A vector of \code{n_clusters} non-negative scalar specifying the
+#'   scale (precision) parameters of the Mallows mixture components.
+#' @param weights A vector of \code{n_clusters} non-negative scalars specifying
+#'   the mixture weights.
+#' @param metric Character string specifying the distance measure to use.
+#'   Available options are \code{"kendall"}, \code{"cayley"}, \code{"hamming"},
+#'   \code{"ulam"} for \code{n_items<=95}, \code{"footrule"} for
+#'   \code{n_items<=50} and \code{"spearman"} for \code{n_items<=14}.
 #' @param rankings A matrix with observed rankings in each row.
-#' @param obs_freq A vector of observation frequencies (weights) to apply to each row in \code{rankings}.
-#'   This can speed up computation if a large number of assessors share the same
-#'   rank pattern. Defaults to \code{NULL}, which means that each row of
-#'   \code{rankings} is multiplied by 1. If provided, \code{obs_freq} must have
-#'   the same number of elements as there are rows in \code{rankings}, and
-#'   \code{rankings} cannot be \code{NULL}.
-#' @param log A logical; if TRUE, the log-likelihood value is returned. Default is \code{FALSE}.
+#' @param obs_freq A vector of observation frequencies (weights) to apply to
+#'   each row in \code{rankings}. This can speed up computation if a large
+#'   number of assessors share the same rank pattern. Defaults to \code{NULL},
+#'   which means that each row of \code{rankings} is multiplied by 1. If
+#'   provided, \code{obs_freq} must have the same number of elements as there
+#'   are rows in \code{rankings}, and \code{rankings} cannot be \code{NULL}.
+#' @param log A logical; if TRUE, the log-likelihood value is returned. Default
+#'   is \code{FALSE}.
 #'
-#' @return The likelihood or the log-likelihood value corresponding to one or more observed complete rankings under the Mallows mixture rank model with distance specified by the \code{metric} argument.
+#' @return The likelihood or the log-likelihood value corresponding to one or
+#'   more observed complete rankings under the Mallows mixture rank model with
+#'   distance specified by the \code{metric} argument.
 #' @export
 #'
 #' @example /inst/examples/Lik_DB_MIX_example.R
 #'
-Lik_DB_MIX <- function(rho,alpha,weights,metric,rankings,obs_freq=NULL,log=FALSE){
+Lik_DB_MIX <- function(rho, alpha, weights, metric,
+                       rankings, obs_freq = NULL, log = FALSE){
 
   if(!is.matrix(rankings)){
     rankings <- matrix(rankings, nrow = 1)
   }
 
   if(!is.null(obs_freq)){
-    if(is.null(rankings)){
-      stop("rankings matrix must be provided when obs_freq are provided")
-    }
     if(nrow(rankings) != length(obs_freq)){
       stop("obs_freq must be of same length as the number of rows in rankings")
     }
--- inst/examples/Lik_DB_MIX_example.R
+++ inst/examples/Lik_DB_MIX_example.R
@@ -1,16 +1,47 @@
 # Simulate a sample from a Mallows model with the Kendall distance
 
 n_items <- 5
-mydata <- sample_mallows(n_samples=100,rho0=1:n_items,alpha0=10,metric="kendall")
+mydata <- sample_mallows(
+  n_samples = 100,
+  rho0 = 1:n_items,
+  alpha0 = 10,
+  metric="kendall")
 
 # Compute the likelihood and log-likelihood values under the true model...
+Lik_DB_MIX(
+  rho = rbind(1:n_items,1:n_items),
+  alpha = c(2,2),
+  weights = c(0.5,0.5),
+  metric = "kendall",
+  rankings = mydata
+  )
 
-Lik_DB_MIX(rho=rbind(1:n_items,1:n_items),alpha=c(2,2),weights=c(0.5,0.5),metric="kendall",rankings=mydata)
-Lik_DB_MIX(rho=rbind(1:n_items,1:n_items),alpha=c(2,2),weights=c(0.5,0.5),metric="kendall",rankings=mydata,log=TRUE)
+Lik_DB_MIX(
+  rho = rbind(1:n_items, 1:n_items),
+  alpha = c(2, 2),
+  weights = c(0.5, 0.5),
+  metric = "kendall",
+  rankings = mydata,
+  log = TRUE
+  )
 
 # or equivalently, by using the frequency distribution
-
 freq_distr <- rank_freq_distr(mydata)
-Lik_DB_MIX(rho=rbind(1:n_items,1:n_items),alpha=c(2,2),weights=c(0.5,0.5),metric="kendall",rankings=freq_distr[,1:n_items],obs_freq=freq_distr[,n_items+1])
-Lik_DB_MIX(rho=rbind(1:n_items,1:n_items),alpha=c(2,2),weights=c(0.5,0.5),metric="kendall",rankings=freq_distr[,1:n_items],obs_freq=freq_distr[,n_items+1],log=TRUE)
+Lik_DB_MIX(
+  rho = rbind(1:n_items,1:n_items),
+  alpha = c(2,2),
+  weights = c(0.5, 0.5),
+  metric = "kendall",
+  rankings = freq_distr[, 1:n_items],
+  obs_freq = freq_distr[,n_items+1]
+  )
+Lik_DB_MIX(
+  rho = rbind(1:n_items, 1:n_items),
+  alpha = c(2, 2),
+  weights=c(0.5, 0.5),
+  metric = "kendall",
+  rankings = freq_distr[, 1:n_items],
+  obs_freq = freq_distr[, n_items+1],
+  log=TRUE
+  )
 
