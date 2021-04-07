b9d8f1b15c7147da3eda5b8b6cda2c83c2105917
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-04-07 08:30:38 +0200

    Whitespace removal

--- R/smc_metropolis_hastings_alpha.R
+++ R/smc_metropolis_hastings_alpha.R
@@ -1,17 +1,17 @@
 #' @title Metropolis-Hastings Alpha
-#'  @decscription Function to perform Metropolis-Hastings for new alpha under the Mallows model
-#'   @param alpha Numeric value og the scale parameter
-#'   @param n_items Integer is the number of items in a ranking
-#'   @param rankings A matrix of size \eqn{N }\eqn{\times}{x}\eqn{ n_items} of
-#'   rankings in each row. Alternatively, if \eqn{N} equals 1, \code{rankings}
-#'   can be a vector.
-#'   @param metric A character string specifying the distance metric to use in the
-#'   Bayesian Mallows Model. Available options are \code{"footrule"},
-#'   \code{"spearman"}, \code{"cayley"}, \code{"hamming"}, \code{"kendall"}, and
-#'   \code{"ulam"}.
-#'   @param rho Numeric vector specifying the consensus ranking
-#'   @param logz_estimate Estimate of the partition function, computed with
-#'   \code{\link{estimate_partition_function}} in the BayesMallow R package {estimate_partition_function}.
+#' @decscription Function to perform Metropolis-Hastings for new alpha under the Mallows model
+#' @param alpha Numeric value og the scale parameter
+#' @param n_items Integer is the number of items in a ranking
+#' @param rankings A matrix of size \eqn{N }\eqn{\times}{x}\eqn{ n_items} of
+#' rankings in each row. Alternatively, if \eqn{N} equals 1, \code{rankings}
+#' can be a vector.
+#' @param metric A character string specifying the distance metric to use in the
+#' Bayesian Mallows Model. Available options are \code{"footrule"},
+#' \code{"spearman"}, \code{"cayley"}, \code{"hamming"}, \code{"kendall"}, and
+#' \code{"ulam"}.
+#' @param rho Numeric vector specifying the consensus ranking
+#' @param logz_estimate Estimate of the partition function, computed with
+#' \code{\link{estimate_partition_function}} in the BayesMallow R package {estimate_partition_function}.
 #' @return \code{alpha} or \code{alpha_prime}: Numeric value to be used as the proposal of a new alpha
 #'' @importFrom stats dexp rlnorm runif
 metropolis_hastings_alpha <- function(alpha, n_items, rankings, metric, rho, logz_estimate) {
