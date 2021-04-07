ad0dfacb44470e5d9272587f44ac4e0d5c822e10
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-04-07 08:12:18 +0200

    Added documentation for SMC functions (solves #66)

--- R/smc_get_mallows_loglik.R
+++ R/smc_get_mallows_loglik.R
@@ -1,19 +1,17 @@
+#' @title Get Mallows log-likelihood
+#' @description Calculates the Mallows log-likelihood given a set of rankings and a given rank sequence
+#' @param alpha Numeric value of the scale parameter
+#' @param rho A ranking sequence
+#' @param n_items Integer is the number of items in a ranking
+#'   A matrix of size \eqn{N }\eqn{\times}{x}\eqn{ n_items} of
+#'   rankings in each row. Alternatively, if \eqn{N} equals 1, \code{rankings}
+#'   can be a vector.
+#'   @param metric Character string specifying the distance measure to use.
+#'   Available options are \code{"kendall"}, \code{"cayley"}, \code{"hamming"},
+#'   \code{"ulam"}, \code{"footrule"} and \code{"spearman"}.
+#'  @return Mallows log-likelihood
 get_mallows_loglik <- function(alpha, rho, n_items, rankings, metric){
 
-  #   @Description Calculates the Mallows log-likelihood given a set of rankings and a given rank sequence
-
-  # INPUT:
-  #   @param alpha Numeric value of the scale parameter
-  #   @param rho A ranking sequence
-  #   @param n_items Integer is the number of items in a ranking
-  #   A matrix of size \eqn{N }\eqn{\times}{x}\eqn{ n_items} of
-  #   rankings in each row. Alternatively, if \eqn{N} equals 1, \code{rankings}
-  #   can be a vector.
-  #   @param metric Character string specifying the distance measure to use.
-  #   Available options are \code{"kendall"}, \code{"cayley"}, \code{"hamming"},
-  #   \code{"ulam"}, \code{"footrule"} and \code{"spearman"}.
-
-  # OUTPUT: Mallows log-likelihood
 
   sum_distance = 0
   num_rankings = dim(rankings)[1]
--- R/smc_leap_and_shift_probs.R
+++ R/smc_leap_and_shift_probs.R
@@ -1,17 +1,17 @@
+#' @title Leap and Shift Probabilities
+#'  @description Determine the new Calculates transition probabilities for proposing a new rho
+#'   @param rho A ranking sequence
+#'   @param leap_size Integer specifying the step size of the leap-and-shift
+#'   proposal distribution.
+#'   @param n_items Integer is the number of items in a ranking
+
+#' @return A list containing:
+#' \itemize{
+#'   \item \code{rho_prime} A ranking sequence proposed consensus ranking
+#'   \item \code{forwards_prob} Numeric value to account for transition probability from rho to rho_prime
+#'   \item \code{backwards_prob} Numeric Value to account for the transition probability from \code{rho_prime} to \code{rho}
+#' }
 leap_and_shift_probs<- function(rho, leap_size, n_items){
-
-  # @description Determine the new Calculates transition probabilities for proposing a new rho
-
-  # INPUT:
-  #   @param rho A ranking sequence
-  #   @param leap_size Integer specifying the step size of the leap-and-shift
-  #   proposal distribution.
-  #   @param n_items Integer is the number of items in a ranking
-
-  # OUTPUT:  (List)
-  #   rho_prime A ranking sequence proposed consensus ranking
-  #   forwards probability Numeric value to account for transition probability from rho to rho_prime
-  #   backwards probability Numeric Value to account for the transition probability from rho_prime to rho
 
   # draw u uniformly from {1,...,n}
   u = sample.int(n_items, 1)
