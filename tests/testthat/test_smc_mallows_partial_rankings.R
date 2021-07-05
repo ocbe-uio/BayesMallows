8011f836884cd208ee74970f5acf4031e1cbdf17
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-07-05 09:37:32 +0200

    Fixes tests (#109)

--- tests/testthat/test_smc_mallows_partial_rankings.R
+++ tests/testthat/test_smc_mallows_partial_rankings.R
@@ -29,7 +29,6 @@ logz_estimate <- estimate_partition_function(
 samples <- sushi_rankings[1:100, ]
 samples[samples > 5] <- NA
 
-
 #######################################
 # Bayesmallows MCMC Results
 #######################################
@@ -41,41 +40,16 @@ bayesmallows_mcmc$burnin <- 1000
 items <- sample(1:n_items, 5, replace = F)
 items <- sort(items)
 
-# plot trace - rho
-# p <- assess_convergence(bayesmallows_mcmc, parameter = "rho", items = items)  +
-#   ggtitle("Trace plot for MCMC algorithm in BayesMallows R package") +
-#   theme(plot.title = element_text(hjust = 0.5))
-# p
-
-# heatplot
-bayesmallows_rho_matrix <- matrix(bayesmallows_mcmc$rho$value, ncol = n_items, nrow = nmc, byrow = TRUE)
-bayesmallows_heatmat_rho <- BayesMallows:::heatMat(mcmcOutput = bayesmallows_rho_matrix, burnin = bayesmallows_mcmc$burnin, t_rank = rho_0)
-# bayesmallows_heatplot = BayesMallows:::heatPlot_fixed(mat = bayesmallows_heatmat_rho,t_rank = rho_0)
-
-
-# posterior intervals for rho
-compute_posterior_intervals(bayesmallows_mcmc, parameter = "rho")
-
-compute_consensus(model_fit = bayesmallows_mcmc, type = "CP", burnin = bayesmallows_mcmc$burnin)
-compute_consensus(model_fit = bayesmallows_mcmc, type = "MAP", burnin = bayesmallows_mcmc$burnin)
-
-# plot trace - alpha
-# assess_convergence(bayesmallows_mcmc, parameter = "alpha")+
-#   ggtitle("Trace plot for MCMC algorithm in BayesMallows R package") +
-#   theme(plot.title = element_text(hjust = 0.5))
-# ggsave("bayesmallows_alpha_traceplot.png", height = 6, width = 7)
-
-# bayesmallows_alpha_posterior_plot = plot(bayesmallows_mcmc, parameter = "alpha", burnin = bayesmallows_mcmc$burnin) +
-#   ggtitle("BayesMallows R package") + theme(plot.title = element_text(hjust = 0.5))
-# bayesmallows_alpha_posterior_plot
-# ggsave("bayesmallows_alpha_posterior_plot.png", height = 6, width = 7)
-
-# determine posterior estimates - alpha
-bayesmallows_alpha_posterior_intervals <- compute_posterior_intervals(bayesmallows_mcmc, parameter = "alpha")
-bayesmallows_alpha_posterior_intervals
-
-
-
+test_that('BayesMallows MCMC Results are OK', {
+    rho_cp <- compute_consensus(model_fit = bayesmallows_mcmc, type = "CP", burnin = bayesmallows_mcmc$burnin)
+    rho_map <- compute_consensus(model_fit = bayesmallows_mcmc, type = "MAP", burnin = bayesmallows_mcmc$burnin)
+    post_rho <- compute_posterior_intervals(bayesmallows_mcmc, parameter = "rho")
+    post_alpha <- compute_posterior_intervals(bayesmallows_mcmc, parameter = "alpha")
+    expect_equal(dim(post_rho), c(10, 7))
+    expect_equal(dim(rho_cp), c(10, 3))
+    expect_equal(dim(rho_map), c(10, 3))
+    expect_equal(dim(post_alpha), c(1, 6))
+})
 
 ###############################
 # SMC Analysis (alpha unknown)
@@ -90,7 +64,6 @@ alpha_prop_sd <- 0.5
 lambda <- 0.15
 alpha_max <- 1e6
 aug_method <- "random"
-# aug_method = "pseudolikelihood"
 
 test <- smc_mallows_new_users_partial(
   R_obs = samples, n_items = n_items, metric = metric,
@@ -100,30 +73,16 @@ test <- smc_mallows_new_users_partial(
   lambda = lambda, alpha_max = alpha_max, aug_method = aug_method
 )
 
-
-# heatplot for rho
-
-# plot_rho_heatplot(output = test$rho_samples[,,Time+1], nmc = N, burnin = 0, n_items = n_items, rho_0 = rho_0)
-
-# posterior confidence intervals for rho
-
-BayesMallows:::compute_posterior_intervals_rho(output = test$rho_samples[, , Time + 1], nmc = N, burnin = 0)
-
-# MAP AND CP consensus ranking estimates
-rho_cp <- BayesMallows:::compute_rho_consensus(output = test$rho_samples[, , Time + 1], nmc = N, burnin = 0, C = 1, type = "CP")
-rho_cp
-rho_map <- BayesMallows:::compute_rho_consensus(output = test$rho_samples[, , Time + 1], nmc = N, burnin = 0, C = 1, type = "MAP")
-rho_map
-
-
-# posterior for alpha
-# plot_alpha_posterior(output = test$alpha_samples[,Time+1], nmc = N, burnin = 0)
-
-# posterior confidence intervals
-alpha_posterior_intervals <- BayesMallows:::compute_posterior_intervals_alpha(output = test$alpha_samples[, Time + 1], nmc = N, burnin = 0)
-
-
-
+test_that('SMC analysis (alpha unknown) results are OK', {
+    rho_cp <- compute_rho_consensus(output = test$rho_samples[, , Time + 1], nmc = N, burnin = 0, C = 1, type = "CP")
+    rho_map <- compute_rho_consensus(output = test$rho_samples[, , Time + 1], nmc = N, burnin = 0, C = 1, type = "MAP")
+    post_rho <- compute_posterior_intervals_rho(output = test$rho_samples[, , Time + 1], nmc = N, burnin = 0)
+    post_alpha <- compute_posterior_intervals_alpha(output = test$alpha_samples[, Time + 1], nmc = N, burnin = 0)
+    expect_equal(dim(rho_cp), c(10, 3))
+    expect_equal(dim(rho_map), c(12, 3))
+    expect_equal(dim(post_rho), c(10, 7))
+    expect_equal(dim(post_alpha), c(1, 6))
+})
 
 ###############################
 # SMC Analysis (alpha fixed)
@@ -138,14 +97,11 @@ test <- smc_mallows_new_users_partial_alpha_fixed(
   num_new_obs = num_new_obs, aug_method = aug_method, alpha = alpha_0
 )
 
-# heatplot for rho
-# plot_rho_heatplot_partial(output = test$rho_samples[,,Time+1], nmc = N, burnin = 0, n_items = n_items, rho_0 = rho_0)
-
-# posterior confidence intervals for rho
-compute_posterior_intervals_rho(output = test$rho_samples[, , Time + 1], nmc = N, burnin = 0)
-
-# MAP AND CP consensus ranking estimates
-rho_cp <- compute_rho_consensus(output = test$rho_samples[, , Time + 1], nmc = N, burnin = 0, C = 1, type = "CP")
-rho_cp
-rho_map <- compute_rho_consensus(output = test$rho_samples[, , Time + 1], nmc = N, burnin = 0, C = 1, type = "MAP")
-rho_map
+test_that('SMC analysis (alpha fixed) results are OK', {
+    rho_cp <- compute_rho_consensus(output = test$rho_samples[, , Time + 1], nmc = N, burnin = 0, C = 1, type = "CP")
+    rho_map <- compute_rho_consensus(output = test$rho_samples[, , Time + 1], nmc = N, burnin = 0, C = 1, type = "MAP")
+    post_rho <- compute_posterior_intervals_rho(output = test$rho_samples[, , Time + 1], nmc = N, burnin = 0)
+    expect_equal(dim(rho_cp), c(10, 3))
+    expect_equal(dim(rho_map), c(12, 3))
+    expect_equal(dim(post_rho), c(10, 7))
+})
