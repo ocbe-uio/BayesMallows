b7ecd459b10509f87f127772cca099ae2842f663
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-07-12 09:48:51 +0200

    Added get_sample_probabilities to smc.h

--- src/smc.h
+++ src/smc.h
@@ -7,5 +7,6 @@ double get_mallows_loglik(double, arma::vec, int, arma::mat, std::string);
 Rcpp::List leap_and_shift_probs(arma::vec, int, int);
 arma::vec metropolis_hastings_rho(double, int, arma::mat, std::string, arma::vec, int);
 double metropolis_hastings_alpha(double, int, arma::mat, std::string, arma::vec, const Rcpp::Nullable<arma::vec>, double, double, double);
+arma::vec get_sample_probabilities(arma::vec, double, arma::vec, std::string, int);
 
 #endif
