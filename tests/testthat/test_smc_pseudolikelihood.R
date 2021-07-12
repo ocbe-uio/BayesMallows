3eaff0dd2d15222f6090727986084203cf409314
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-07-12 10:38:12 +0200

    Updated unit test expectations

--- tests/testthat/test_smc_pseudolikelihood.R
+++ tests/testthat/test_smc_pseudolikelihood.R
@@ -37,12 +37,10 @@ test_1_forward = calculate_forward_probability(item_ordering = item_ordering, pa
                                                remaining_set = remaining_set, rho = rho, alpha = alpha,
                                                n_items = n_items, metric = metric)
 
-
 current_ranking = c(1,2,6,5,4,3)
 test_1_backward_a= calculate_backward_probability(item_ordering = item_ordering, partial_ranking = partial_ranking,
                                                   current_ranking = current_ranking, remaining_set = remaining_set, rho = rho,
                                                   alpha = alpha, n_items = n_items, metric = metric)
-
 
 current_ranking = test_1_forward$aug_ranking
 test_1_backward_b = calculate_backward_probability(item_ordering = item_ordering, partial_ranking = partial_ranking,
@@ -50,7 +48,7 @@ test_1_backward_b = calculate_backward_probability(item_ordering = item_ordering
                                                    alpha = alpha, n_items = n_items, metric = metric)
 
 test_that('calculations of forward and backward probabilities', {
-    expect_equal(test_1_forward$aug_ranking, c(1, 2, 3, 5, 4, 6))
+    expect_equal(test_1_forward$aug_ranking, matrix(c(1, 2, 3, 5, 4, 6)))
     expect_equal(test_1_forward$forward_prob, 0.07205734)
     expect_equal(test_1_backward_a, 0.01360987)
     expect_equal(test_1_backward_b, 0.07205734)
@@ -76,12 +74,12 @@ test_that('M-H aug ranking pseudo works', {
     R_curr = c(1,2,3,4,5,6)
     R_obs = c(1,2,3,NA,NA,NA)
     test_2 = metropolis_hastings_aug_ranking_pseudo(alpha, rho, n_items, partial_ranking = R_obs, current_ranking = R_curr, metric)
-    expect_equal(test_2, c(1, 2, 3, 5, 4, 6))
+    expect_equal(test_2, matrix(c(1, 2, 3, 5, 4, 6)))
     expect_equal(all(test_2 == R_curr), FALSE)
     R_curr = c(1,2,6,5,4,3)
     R_obs = c(1,2,NA,NA,NA,NA)
     test_3 =  metropolis_hastings_aug_ranking_pseudo(alpha, rho, n_items, partial_ranking = R_obs, current_ranking = R_curr, metric)
-    expect_equal(test_3, c(1, 2, 5, 6, 4, 3))
+    expect_equal(test_3, matrix(c(1, 2, 5, 6, 4, 3)))
     expect_equal(all(test_3 == R_curr), FALSE)
 })
 
