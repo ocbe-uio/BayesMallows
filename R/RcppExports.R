4faf3fa5c0cbf9e5f68171093731b89ac1034d4c
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-09-14 13:27:05 +0200

    Translated smc_mallows_new_users_partial (#107)
    - Added C++ skeleton of smc_mallows_new_users_partial
    - Translated function

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -406,6 +406,36 @@ smc_mallows_new_users_complete <- function(R_obs, n_items, metric, leap_size, N,
     .Call(`_BayesMallows_smc_mallows_new_users_complete`, R_obs, n_items, metric, leap_size, N, Time, mcmc_kernel_app, num_new_obs, logz_estimate, verbose)
 }
 
+#' @title SMC-Mallows new users partial
+#' @description Function to perform resample-move SMC algorithm where we receive new users with complete rankings
+#' at each time step
+#' @param R_obs Matrix containing the full set of observed rankings of size n_assessors by n_items
+#' @param n_items Integer is the number of items in a ranking
+#' @param metric A character string specifying the distance metric to use in the
+#' Bayesian Mallows Model. Available options are \code{"footrule"},
+#' \code{"spearman"}, \code{"cayley"}, \code{"hamming"}, \code{"kendall"}, and
+#' \code{"ulam"}.
+#' @param leap_size leap_size Integer specifying the step size of the leap-and-shift
+#' proposal distribution
+#' @param N Integer specifying the number of particles
+#' @param Time Integer specifying the number of time steps in the SMC algorithm
+#' @param logz_estimate Estimate of the partition function, computed with
+#' \code{\link{estimate_partition_function}} in the BayesMallow R package {estimate_partition_function}.
+#' @param mcmc_kernel_app Interger value for the number of applications we apply the MCMC move kernel
+#' @param num_new_obs Integer value for the number of new observations (complete rankings) for each time step
+#' @param alpha_prop_sd Numeric value of the standard deviation of the prior distribution for alpha
+#' @param lambda Strictly positive numeric value specifying the rate parameter
+#' of the truncated exponential prior distribution of alpha.
+#' @param alpha_max  Maximum value of alpha in the truncated exponential
+#' prior distribution.
+#' @param aug_method A character string specifying the approach for filling in the missing data, options are "pseudolikelihood" or "random"
+#' @param verbose Logical specifying whether to print out the progress of the
+#' SMC-Mallows algorithm. Defaults to \code{FALSE}.
+#' @return a set of particles each containing a value of rho and alpha
+smc_mallows_new_users_partial_Cpp <- function(R_obs, n_items, metric, leap_size, N, Time, mcmc_kernel_app, num_new_obs, alpha_prop_sd, lambda, alpha_max, aug_method, logz_estimate = NULL, verbose = FALSE) {
+    .Call(`_BayesMallows_smc_mallows_new_users_partial_Cpp`, R_obs, n_items, metric, leap_size, N, Time, mcmc_kernel_app, num_new_obs, alpha_prop_sd, lambda, alpha_max, aug_method, logz_estimate, verbose)
+}
+
 #' @title Metropolis-Hastings Alpha
 #' @description Function to perform Metropolis-Hastings for new rho under
 #'   the Mallows model with footrule distance metric!
--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -327,6 +327,30 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
+// smc_mallows_new_users_partial_Cpp
+Rcpp::List smc_mallows_new_users_partial_Cpp(arma::mat& R_obs, unsigned int& n_items, std::string& metric, int& leap_size, unsigned int& N, unsigned int Time, int& mcmc_kernel_app, unsigned int& num_new_obs, double alpha_prop_sd, double lambda, double alpha_max, std::string& aug_method, const Rcpp::Nullable<arma::vec>& logz_estimate, bool verbose);
+RcppExport SEXP _BayesMallows_smc_mallows_new_users_partial_Cpp(SEXP R_obsSEXP, SEXP n_itemsSEXP, SEXP metricSEXP, SEXP leap_sizeSEXP, SEXP NSEXP, SEXP TimeSEXP, SEXP mcmc_kernel_appSEXP, SEXP num_new_obsSEXP, SEXP alpha_prop_sdSEXP, SEXP lambdaSEXP, SEXP alpha_maxSEXP, SEXP aug_methodSEXP, SEXP logz_estimateSEXP, SEXP verboseSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< arma::mat& >::type R_obs(R_obsSEXP);
+    Rcpp::traits::input_parameter< unsigned int& >::type n_items(n_itemsSEXP);
+    Rcpp::traits::input_parameter< std::string& >::type metric(metricSEXP);
+    Rcpp::traits::input_parameter< int& >::type leap_size(leap_sizeSEXP);
+    Rcpp::traits::input_parameter< unsigned int& >::type N(NSEXP);
+    Rcpp::traits::input_parameter< unsigned int >::type Time(TimeSEXP);
+    Rcpp::traits::input_parameter< int& >::type mcmc_kernel_app(mcmc_kernel_appSEXP);
+    Rcpp::traits::input_parameter< unsigned int& >::type num_new_obs(num_new_obsSEXP);
+    Rcpp::traits::input_parameter< double >::type alpha_prop_sd(alpha_prop_sdSEXP);
+    Rcpp::traits::input_parameter< double >::type lambda(lambdaSEXP);
+    Rcpp::traits::input_parameter< double >::type alpha_max(alpha_maxSEXP);
+    Rcpp::traits::input_parameter< std::string& >::type aug_method(aug_methodSEXP);
+    Rcpp::traits::input_parameter< const Rcpp::Nullable<arma::vec>& >::type logz_estimate(logz_estimateSEXP);
+    Rcpp::traits::input_parameter< bool >::type verbose(verboseSEXP);
+    rcpp_result_gen = Rcpp::wrap(smc_mallows_new_users_partial_Cpp(R_obs, n_items, metric, leap_size, N, Time, mcmc_kernel_app, num_new_obs, alpha_prop_sd, lambda, alpha_max, aug_method, logz_estimate, verbose));
+    return rcpp_result_gen;
+END_RCPP
+}
 // metropolis_hastings_alpha
 double metropolis_hastings_alpha(double alpha, int n_items, arma::mat rankings, std::string metric, arma::vec rho, const Rcpp::Nullable<arma::vec> logz_estimate, double alpha_prop_sd, double lambda, double alpha_max);
 RcppExport SEXP _BayesMallows_metropolis_hastings_alpha(SEXP alphaSEXP, SEXP n_itemsSEXP, SEXP rankingsSEXP, SEXP metricSEXP, SEXP rhoSEXP, SEXP logz_estimateSEXP, SEXP alpha_prop_sdSEXP, SEXP lambdaSEXP, SEXP alpha_maxSEXP) {
@@ -416,6 +440,7 @@ static const R_CallMethodDef CallEntries[] = {
     {"_BayesMallows_get_sample_probabilities", (DL_FUNC) &_BayesMallows_get_sample_probabilities, 5},
     {"_BayesMallows_leap_and_shift_probs", (DL_FUNC) &_BayesMallows_leap_and_shift_probs, 3},
     {"_BayesMallows_smc_mallows_new_users_complete", (DL_FUNC) &_BayesMallows_smc_mallows_new_users_complete, 10},
+    {"_BayesMallows_smc_mallows_new_users_partial_Cpp", (DL_FUNC) &_BayesMallows_smc_mallows_new_users_partial_Cpp, 14},
     {"_BayesMallows_metropolis_hastings_alpha", (DL_FUNC) &_BayesMallows_metropolis_hastings_alpha, 9},
     {"_BayesMallows_metropolis_hastings_aug_ranking", (DL_FUNC) &_BayesMallows_metropolis_hastings_aug_ranking, 6},
     {"_BayesMallows_metropolis_hastings_aug_ranking_pseudo", (DL_FUNC) &_BayesMallows_metropolis_hastings_aug_ranking_pseudo, 6},
