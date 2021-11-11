0871c15874ba8844e12c724c931e6ed57e39928e
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-11-11 10:13:45 +0100

    Moved methods to same file (#80)
    Moved all methods of `compute_posterior_intervals()` and
    `.compute_posterior_intervals()` to the same file (easier to manage).

--- R/compute_posterior_intervals.R
+++ R/compute_posterior_intervals.R
@@ -19,6 +19,8 @@
 #' @param decimals Integer specifying the number of decimals to include
 #' in posterior intervals and the mean and median. Defaults to \code{3}.
 #'
+#' @param ... other arguments passed to methods.
+#'
 #' @details This function computes both the Highest Posterior Density Interval (HPDI),
 #' which may be discontinuous for bimodal distributions, and
 #' the central posterior interval, which is simply defined by the quantiles of the posterior
@@ -37,12 +39,13 @@ compute_posterior_intervals <- function(model_fit, ...) {
   UseMethod("compute_posterior_intervals")
 }
 .compute_posterior_intervals <- function(df, ...) {
-  UseMethod((".compute_posterior_intervals"))
+  UseMethod(".compute_posterior_intervals")
 }
 
-compute_posterior_intervals.BayesMallows <- function(model_fit, burnin = model_fit$burnin,
-                                        parameter = "alpha", level = 0.95,
-                                        decimals = 3L){
+compute_posterior_intervals.BayesMallows <- function(
+  model_fit, burnin = model_fit$burnin, parameter = "alpha", level = 0.95,
+  decimals = 3L
+) {
   stopifnot(class(model_fit) == "BayesMallows")
 
   if(is.null(burnin)){
@@ -76,8 +79,48 @@ compute_posterior_intervals.BayesMallows <- function(model_fit, burnin = model_f
   return(df)
 }
 
+compute_posterior_intervals.SMCMallows <- function(
+  model_fit, burnin = model_fit$burnin, parameter = "alpha", level = 0.95,
+  decimals = 3L
+) {
+  if (is.null(burnin)) {
+    stop("Please specify the burnin.")
+  }
 
-.compute_posterior_intervals.BayesMallows <- function(df, parameter, level, decimals, discrete = FALSE){
+  stopifnot(burnin < model_fit$nmc)
+  stopifnot(parameter %in% c("alpha", "rho", "cluster_probs", "cluster_assignment"))
+  stopifnot(level > 0 && level < 1)
+
+
+  if (burnin != 0) {
+    df <- dplyr::filter(model_fit, .data$iteration > burnin) # removed model_fit[[parameter]]
+  } else {
+    df <- model_fit
+  }
+
+  if (parameter == "alpha" || parameter == "cluster_probs") {
+    df <- dplyr::group_by(df, .data$cluster)
+    class(df) <- c("SMCMallows", "grouped_df", "tbl_df", "tbl", "data.frame")
+    df <- .compute_posterior_intervals(df, parameter, level, decimals)
+  } else if (parameter == "rho") {
+    decimals <- 0
+    df <- dplyr::group_by(df, .data$cluster, .data$item)
+    class(df) <- c("SMCMallows", "grouped_df", "tbl_df", "tbl", "data.frame")
+    df <- .compute_posterior_intervals(df, parameter, level, decimals, discrete = TRUE)
+  }
+
+  df <- dplyr::ungroup(df)
+
+  if (model_fit$n_clusters[1] == 1) {
+    df <- dplyr::select(df, -.data$cluster)
+  }
+
+  return(df)
+}
+
+.compute_posterior_intervals.BayesMallows <- function(
+  df, parameter, level, decimals, discrete = FALSE
+){
   dplyr::do(df, {
     format <- paste0("%.", decimals, "f")
 
@@ -112,6 +155,69 @@ compute_posterior_intervals.BayesMallows <- function(model_fit, burnin = model_f
 
       hpdi[] <- sprintf(format, hpdi)
       if(is.matrix(hpdi)){
+        # Discontinous case
+        hpdi <- paste(apply(hpdi, 1, function(x) paste0("[", x[[1]], ",", x[[2]], "]")))
+      } else {
+        # Continuous case
+        hpdi <- paste0("[", hpdi[[1]], ",", hpdi[[2]], "]")
+      }
+    }
+
+
+    central <- unique(stats::quantile(.data$value, probs = c((1 - level) / 2, level + (1 - level) / 2)))
+    central <- sprintf(format, central)
+    central <- paste0("[", paste(central, collapse = ","), "]")
+
+    dplyr::tibble(
+      parameter = parameter,
+      mean = posterior_mean,
+      median = posterior_median,
+      conf_level = paste(level * 100, "%"),
+      hpdi = hpdi,
+      central_interval = central
+    )
+  })
+}
+
+# same as compute_posterior_intervals, but removed the bayesmallows object and
+# some other columns
+.compute_posterior_intervals.SMCMallows <- function(
+  df, parameter, level, decimals, discrete = FALSE
+) {
+  dplyr::do(df, {
+    format <- paste0("%.", decimals, "f")
+
+    posterior_mean <- round(base::mean(.data$value), decimals)
+    posterior_median <- round(stats::median(.data$value), decimals)
+
+    if (discrete) {
+      df <- dplyr::group_by(.data, .data$value)
+      df <- dplyr::summarise(df, n = dplyr::n())
+      df <- dplyr::arrange(df, dplyr::desc(.data$n))
+      df <- dplyr::mutate(df,
+        cumprob = cumsum(.data$n) / sum(.data$n),
+        lagcumprob = dplyr::lag(.data$cumprob, default = 0)
+      )
+
+      df <- dplyr::filter(df, .data$lagcumprob <= level)
+
+      values <- sort(dplyr::pull(df, .data$value))
+
+      # Find contiguous regions
+      breaks <- c(0, which(diff(values) != 1), length(values))
+
+      hpdi <- purrr::map(seq(length(breaks) - 1), function(.x, values, breaks) {
+        vals <- values[(breaks[.x] + 1):breaks[.x + 1]]
+        vals <- unique(c(min(vals), max(vals)))
+        paste0("[", paste(vals, collapse = ","), "]")
+      }, values = values, breaks = breaks)
+
+      hpdi <- paste(hpdi, collapse = ",")
+    } else {
+      hpdi <- HDInterval::hdi(.data$value, credMass = level, allowSplit = TRUE)
+
+      hpdi[] <- sprintf(format, hpdi)
+      if (is.matrix(hpdi)) {
         # Discontinous case
         hpdi <- paste(apply(hpdi, 1, function(x) paste0("[", x[[1]], ",", x[[2]], "]")))
       } else {
