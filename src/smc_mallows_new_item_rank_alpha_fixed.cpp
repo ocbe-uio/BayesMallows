e880819c5968b3f82a5a85de03e427852c0cdb4a
Anja Stein <a.stein1@lancaster.ac.uk>
2022-04-28 11:32:33 +0100

    Update smc_mallows_new_item_rank_alpha_fixed.cpp
    Updated L39 to explain the output.

--- src/smc_mallows_new_item_rank_alpha_fixed.cpp
+++ src/smc_mallows_new_item_rank_alpha_fixed.cpp
@@ -36,7 +36,8 @@ using namespace arma;
 //' the missing data, options are "pseudolikelihood" or "random".
 //' @param verbose Logical specifying whether to print out the progress of the
 //' SMC-Mallows algorithm. Defaults to \code{FALSE}.
-//' @return a 3d matrix containing the samples of rho and alpha from the SMC algorithm
+//' @return a set of particles each containing the values of rho and the effective sample size (ESS) at each iteration of the SMC 
+//' algorithm as well as the set of augmented rankings at the final iteration.
 //' @export
 // [[Rcpp::export]]
 Rcpp::List smc_mallows_new_item_rank_alpha_fixed(
