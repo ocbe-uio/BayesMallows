29b3d8cb238b19b0fa7a673ea79319f2c61c5a04
Anja Stein <a.stein1@lancaster.ac.uk>
2022-04-29 07:07:51 +0200

    Updated @return documentation

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -393,7 +393,8 @@ leap_and_shift_probs <- function(rho, leap_size, n_items) {
 #' @param aug_method A character string specifying the approach for filling in the missing data, options are "pseudolikelihood" or "random"
 #' @param verbose Logical specifying whether to print out the progress of the
 #' SMC-Mallows algorithm. Defaults to \code{FALSE}.
-#' @return a 3d matrix containing the samples of rho and alpha from the SMC algorithm
+#' @return a set of particles each containing the values of rho and alpha and the effective sample size (ESS) at each iteration of the SMC
+#' algorithm as well as the set of augmented rankings at the final iteration.
 #' @export
 smc_mallows_new_item_rank <- function(n_items, R_obs, metric, leap_size, N, Time, logz_estimate, mcmc_kernel_app, alpha_prop_sd, lambda, alpha_max, aug_method, verbose = FALSE) {
     .Call(`_BayesMallows_smc_mallows_new_item_rank`, n_items, R_obs, metric, leap_size, N, Time, logz_estimate, mcmc_kernel_app, alpha_prop_sd, lambda, alpha_max, aug_method, verbose)
--- src/smc_mallows_new_item_rank.cpp
+++ src/smc_mallows_new_item_rank.cpp
@@ -30,7 +30,7 @@ using namespace arma;
 //' @param aug_method A character string specifying the approach for filling in the missing data, options are "pseudolikelihood" or "random"
 //' @param verbose Logical specifying whether to print out the progress of the
 //' SMC-Mallows algorithm. Defaults to \code{FALSE}.
-//' @return a set of particles each containing the values of rho and alpha and the effective sample size (ESS) at each iteration of the SMC 
+//' @return a set of particles each containing the values of rho and alpha and the effective sample size (ESS) at each iteration of the SMC
 //' algorithm as well as the set of augmented rankings at the final iteration.
 //' @export
 // [[Rcpp::export]]
