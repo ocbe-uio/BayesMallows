27dcaf6d75e209f0c6b5f8bd095928202efea948
Øystein Sørensen <oystein_sorensen@hotmail.com>
2022-05-03 13:51:31 +0200

    simplified script for generating beach data

--- data-raw/generate_beach_data.R
+++ data-raw/generate_beach_data.R
@@ -1,30 +1,14 @@
-library(dplyr)
 library(BayesMallows)
 
 # Load the full data
 load("./data-raw/Beaches_data.Rdata")
 
 # We need the data object
-beach_preferences <- data %>%
-  as_tibble() %>%
-  transmute(
-    assessor = as.numeric(assessors),
-    bottom_item = as.numeric(gsub("[[:alpha:]]", "", firstItem)),
-    top_item = as.numeric(gsub("[[:alpha:]]", "", secondItem))
-  )
+data <- as.data.frame(data)
+beach_preferences <- data.frame(
+  assessor = as.numeric(data$assessors),
+  bottom_item = as.numeric(gsub("[[:alpha:]]", "", data$firstItem)),
+  top_item = as.numeric(gsub("[[:alpha:]]", "", data$secondItem))
+)
 
-# Now generate the transitive closure
-beach_tc <- generate_transitive_closure(beach_preferences)
-
-# Find which assessor have given non-transitive relations
-df <- beach_tc %>%
-  mutate(
-    lowest = pmin(bottom_item, top_item),
-    highest = pmax(bottom_item, top_item)
-  ) %>%
-  group_by(assessor, lowest, highest) %>%
-  count()
-
-
-
-devtools::use_data(beach_preferences, overwrite = TRUE)
+usethis::use_data(beach_preferences, overwrite = TRUE)
