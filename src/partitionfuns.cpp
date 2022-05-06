843b03c18bfb5d58483ad1156d5d5032fc868043
Øystein Sørensen <oystein_sorensen@hotmail.com>
2022-05-06 09:51:13 +0200

    replaced code duplication with a function

--- src/partitionfuns.cpp
+++ src/partitionfuns.cpp
@@ -57,17 +57,20 @@ double compute_is_fit(double alpha, vec fit){
   return(logZ);
 }
 
-double logz_cardinalities(const double& alpha, const int& n_items, const vec& cardinalities, const std::string& metric){
-  vec distances;
+vec find_cardinalities(const int& n_items, const std::string& metric){
   if(metric == "footrule"){
-    distances = regspace(0, 2, std::floor(std::pow(static_cast<double>(n_items), 2.) / 2));
+    return(regspace(0, 2, std::floor(std::pow(static_cast<double>(n_items), 2.) / 2)));
   } else if (metric == "spearman"){
-    distances = regspace(0, 2, 2 * binomial_coefficient(n_items + 1, 3));
+    return(regspace(0, 2, 2 * binomial_coefficient(n_items + 1, 3)));
   } else if (metric == "ulam"){
-    distances = regspace(0, 1, n_items - 1);
+    return(regspace(0, 1, n_items - 1));
   } else {
     Rcpp::stop("Cardinalities not implemented for the provided metric.");
   }
+}
+
+double logz_cardinalities(const double& alpha, const int& n_items, const vec& cardinalities, const std::string& metric){
+  vec distances = find_cardinalities(n_items, metric);
   return std::log(sum(cardinalities % exp(-alpha * distances / n_items)));
 }
 
@@ -84,16 +87,7 @@ double logz_cardinalities(const double& alpha, const int& n_items, const vec& ca
 // [[Rcpp::export]]
 double log_expected_dist(const double& alpha, const int& n_items,
                          const arma::vec& cardinalities, const std::string& metric){
-  vec distances;
-  if(metric == "footrule"){
-    distances = regspace(0, 2, std::floor(std::pow(static_cast<double>(n_items), 2.) / 2));
-  } else if (metric == "spearman"){
-    distances = regspace(0, 2, 2 * binomial_coefficient(n_items + 1, 3));
-  } else if (metric == "ulam"){
-    distances = regspace(0, 1, n_items - 1);
-  } else {
-    Rcpp::stop("Cardinalities not implemented for the provided metric.");
-  }
+  vec distances = find_cardinalities(n_items, metric);
   return std::log(sum(distances % cardinalities % exp(-alpha * distances / n_items)))
     -std::log(sum(cardinalities % exp(-alpha * distances / n_items)));
 }
