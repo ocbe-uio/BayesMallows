afaf8736b443ec2f30e14877241675153ad891c1
Øystein Sørensen <oystein_sorensen@hotmail.com>
2022-05-10 15:58:37 +0200

    refactored a little bit more

--- src/smc_metropolis_hastings_aug_ranking.cpp
+++ src/smc_metropolis_hastings_aug_ranking.cpp
@@ -29,48 +29,7 @@ arma::vec metropolis_hastings_aug_ranking(
 	const arma::vec current_ranking,
 	const std::string metric
 ) {
-  // augment incomplete ranks to initialise
-  vec ranks = regspace(1, n_items);
-
-  // find items missing from original observed ranking
-  const uvec unranked_items = find_nonfinite(partial_ranking);
-
-  // find unallocated ranks from original observed ranking
-  vec remaining_set = setdiff_template(current_ranking, partial_ranking);
-
-  // if the observed and augmented ranking are exactly the same then break
-  const bool condition_1 = approx_equal(\
-    partial_ranking, current_ranking, "absdiff", 0.1\
+  return metropolis_hastings_aug_ranking_both(
+    alpha, rho, n_items, partial_ranking, current_ranking, metric, false
   );
-  const bool condition_2 = remaining_set.n_elem == 1;
-  if (condition_1 | condition_2) {
-    return(current_ranking);
-  } else {
-
-    // Subset by element position and set equal to the now permuted remaining set
-    partial_ranking.elem(unranked_items) = shuffle(remaining_set);
-
-    // set the augmented partial ranking as the proposed augmented ranking
-    const vec& proposed_ranking = partial_ranking;
-
-
-     /* MH TIME ------------------------------------------------------------- */
-    // Calculate the log posterior of the current and proposed rankings.
-    // NB the current can usually be stored to save recalculating it, but we're not caring about that yet
-    const double curr_logpost = get_exponent_sum(\
-      alpha, rho, n_items, current_ranking.t(), metric\
-    );
-    const double prop_logpost = get_exponent_sum(\
-      alpha, rho, n_items, proposed_ranking.t(), metric\
-    );
-
-    const double log_acceptance_prob = prop_logpost - curr_logpost;
-
-    if (std::log(Rcpp::as<double>(Rcpp::runif(1, 0, 1))) < log_acceptance_prob) {
-      return(proposed_ranking);
-    } else {
-      return(current_ranking);
-    }
-
-  }
 }
--- src/smc_metropolis_hastings_aug_ranking_pseudo.cpp
+++ src/smc_metropolis_hastings_aug_ranking_pseudo.cpp
@@ -22,7 +22,6 @@ using namespace arma;
 //'         iteration of MCMC chain
 //' @export
 // [[Rcpp::export]]
-
 arma::vec metropolis_hastings_aug_ranking_pseudo(
 	const double alpha,
 	const arma::vec rho,
@@ -31,57 +30,7 @@ arma::vec metropolis_hastings_aug_ranking_pseudo(
 	const arma::vec current_ranking,
 	const std::string metric
 ) {
-  // augment incomplete ranks to initialise
-  vec ranks = regspace(1, n_items);
-
-  // find items missing from original observed ranking
-  const uvec unranked_items = find_nonfinite(partial_ranking);
-
-  // find unallocated ranks from original observed ranking
-  const vec remaining_set = setdiff_template(current_ranking, partial_ranking);
-
-  // if the observed and augmented ranking are exactly the same then break
-  const bool condition_1 = approx_equal(\
-    partial_ranking, current_ranking, "absdiff", 0.1\
+  return metropolis_hastings_aug_ranking_both(
+    alpha, rho, n_items, partial_ranking, current_ranking, metric, true
   );
-  const bool condition_2 = remaining_set.n_elem == 1;
-  if (condition_1 | condition_2) {
-    return(current_ranking);
-  } else {
-    // randomly permute the unranked items to give the order in which they will
-    // be allocated
-    uvec item_ordering = shuffle(unranked_items);
-
-    // Calculate probabilities
-    const Rcpp::List proposal = calculate_forward_probability(\
-      item_ordering, partial_ranking, remaining_set, rho, alpha, n_items,\
-      metric\
-    );
-    const vec& proposed_augmented_ranking = proposal["aug_ranking"];
-    const double& forward_prob = proposal["forward_prob"];
-
-    const double backward_prob = calculate_backward_probability(\
-      item_ordering, partial_ranking, current_ranking, remaining_set, rho,\
-      alpha, n_items, metric\
-    );
-
-     /* MH TIME ------------------------------------------------------------- */
-    // Calculate the log posterior of the current and proposed rankings.
-    // NB the current can usually be stored to save recalculating it, but we're not caring about that yet
-    const double curr_logpost = get_exponent_sum(\
-      alpha, rho, n_items, current_ranking.t(), metric\
-    );
-    const double prop_logpost = get_exponent_sum(\
-      alpha, rho, n_items, proposed_augmented_ranking.t(), metric\
-    );
-
-    const double& log_acceptance_prob = prop_logpost - curr_logpost - std::log(forward_prob) + std::log(backward_prob);
-
-    if (std::log(Rcpp::as<double>(Rcpp::runif(1, 0, 1))) < log_acceptance_prob) {
-      return(proposed_augmented_ranking);
-    } else {
-      return(current_ranking);
-    }
-
-  }
 }
