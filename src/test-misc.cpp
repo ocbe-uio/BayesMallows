5b22c245735fbca65a28cf22470ae4bc03f0627c
Øystein Sørensen <oystein_sorensen@hotmail.com>
2023-11-01 13:27:58 +0100

    removing all old SMC tests, as they need to be re-created

--- src/test-misc.cpp
+++ src/test-misc.cpp
@@ -16,19 +16,19 @@ context("Assorted functions in misc.h") {
   test_that("Combinations of aug_method and metric are validated correctly") {
     // Testing wrong combinations
     expect_error(is_pseudo("wrong", "footrule"));
-    expect_error(is_pseudo("pseudolikelihood", "ulam"));
-    expect_error(is_pseudo("pseudolikelihood", "kendall"));
-    expect_error(is_pseudo("pseudolikelihood", "cayley"));
-    expect_error(is_pseudo("pseudolikelihood", "hamming"));
+    expect_error(is_pseudo("pseudo", "ulam"));
+    expect_error(is_pseudo("pseudo", "kendall"));
+    expect_error(is_pseudo("pseudo", "cayley"));
+    expect_error(is_pseudo("pseudo", "hamming"));
 
     // Testing correct combinations
-    expect_true(is_pseudo("pseudolikelihood", "footrule"));
-    expect_true(is_pseudo("pseudolikelihood", "spearman"));
-    expect_false(is_pseudo("random", "footrule"));
-    expect_false(is_pseudo("random", "spearman"));
-    expect_false(is_pseudo("random", "ulam"));
-    expect_false(is_pseudo("random", "kendall"));
-    expect_false(is_pseudo("random", "cayley"));
-    expect_false(is_pseudo("random", "hamming"));
+    expect_true(is_pseudo("pseudo", "footrule"));
+    expect_true(is_pseudo("pseudo", "spearman"));
+    expect_false(is_pseudo("uniform", "footrule"));
+    expect_false(is_pseudo("uniform", "spearman"));
+    expect_false(is_pseudo("uniform", "ulam"));
+    expect_false(is_pseudo("uniform", "kendall"));
+    expect_false(is_pseudo("uniform", "cayley"));
+    expect_false(is_pseudo("uniform", "hamming"));
   }
 }
