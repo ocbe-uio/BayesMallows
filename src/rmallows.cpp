f17a4bded86ee0b2277572bc3ad3b3e7c8266038
Øystein Sørensen <oystein_sorensen@hotmail.com>
2023-11-20 10:57:25 +0100

    changed to better distance function

--- src/rmallows.cpp
+++ src/rmallows.cpp
@@ -73,7 +73,7 @@ arma::mat rmallows(
 
     // Compute the distances to current and proposed ranks
     double dist_new = get_rank_distance(rho0(indices), rho_proposal(indices), metric);
-    double dist_old = rank_dist_sum(rho0(indices), rho_iter(indices), metric, ones(rho0.n_elem));
+    double dist_old = get_rank_distance(rho0(indices), rho_iter(indices), metric);
 
     // Metropolis-Hastings ratio
     double ratio = - alpha0 / n_items * (dist_new - dist_old) +
