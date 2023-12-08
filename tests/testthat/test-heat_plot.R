27a91b11d9aa590284d06bfca151a4fbf7bc3d1a
Øystein Sørensen <oystein_sorensen@hotmail.com>
2023-12-08 12:03:53 +0100

    added tests for heat_plot

---
+++ tests/testthat/test-heat_plot.R
@@ -0,0 +1,20 @@
+test_that("heat_plot works", {
+  model_fit <- compute_mallows(
+    setup_rank_data(potato_visual),
+    compute_options = set_compute_options(nmc = 200))
+
+  expect_error(heat_plot(model_fit), "Please specify the burnin.")
+  expect_error(heat_plot(model_fit, burnin = 30), "burnin must be <= nmc")
+
+  expect_s3_class(heat_plot(model_fit, burnin = 1), "ggplot")
+  model_fit$burnin <- 1
+  expect_s3_class(heat_plot(model_fit, type = "MAP"), "ggplot")
+
+  model_fit <- compute_mallows(
+    setup_rank_data(cluster_data),
+    model_options = set_model_options(n_clusters = 3),
+    compute_options = set_compute_options(nmc = 20, burnin = 10))
+  expect_error(
+    heat_plot(model_fit),
+    "heat_plot only works for a single cluster")
+})
