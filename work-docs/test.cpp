e1bf7b847c155dcf6caa56ec019e659adf8b74ca
Øystein Sørensen <oystein_sorensen@hotmail.com>
2023-12-11 21:40:55 +0100

    checking platform dependence

--- work-docs/test.cpp
+++ work-docs/test.cpp
@@ -1,9 +1,12 @@
-#include <RcppArmadillo.h>
+#include <RcppArmadilloExtensions/sample.h>
 // [[Rcpp::depends(RcppArmadillo)]]
 
 // [[Rcpp::export]]
-arma::vec test() {
-  return arma::randu(1);
+arma::uvec test() {
+  arma::vec probs(5);
+  probs.fill(.2);
+  arma::uvec inds = arma::regspace<arma::uvec>(0, 4);
+  return Rcpp::RcppArmadillo::sample(inds, inds.size(), true, probs);
 }
 
 
