ecc2b57bbc8bb732c0f305dc9cca370118e4a67d
Øystein Sørensen <oystein_sorensen@hotmail.com>
2023-12-13 09:59:02 +0100

    adding more tests

--- tests/testthat/test-plot_top_k.R
+++ tests/testthat/test-plot_top_k.R
@@ -15,4 +15,11 @@ test_that("predict_top_k works", {
   expect_equal(p$labels$y, "Item")
   expect_equal(p$labels$x, "Assessor")
   expect_s3_class(p, "ggplot")
+
+  model_fit <- compute_mallows(
+    data = setup_rank_data(preferences = beach_preferences),
+    compute_options = set_compute_options(nmc = 10, burnin = 0))
+
+  expect_error(predict_top_k(model_fit), "Please set save_aug = TRUE")
+  expect_error(plot_top_k(model_fit), "Please set save_aug = TRUE")
 })
