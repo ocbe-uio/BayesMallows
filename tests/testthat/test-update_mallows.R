236a21d4aedacb17c81f1db4141ed5409829f2c0
Øystein Sørensen <oystein_sorensen@hotmail.com>
2024-01-26 13:55:23 +0100

    updated failing test and added namespace qualifier

--- tests/testthat/test-update_mallows.R
+++ tests/testthat/test-update_mallows.R
@@ -86,7 +86,7 @@ test_that("update_mallows can start from prior", {
     mod1,
     new_data = setup_rank_data(potato_visual[2, , drop = FALSE])
   )
-  expect_equal(mod2$alpha_samples[[56]], 34.549750670595)
+  expect_equal(mod2$alpha_samples[[56]], 28.68206569895)
 })
 
 test_that("update_mallows handles estimated partition function", {
