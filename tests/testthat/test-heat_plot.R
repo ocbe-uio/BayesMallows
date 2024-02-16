6c9d910f6b5d3caf33defeda8756036939ad1c41
Øystein Sørensen <oystein_sorensen@hotmail.com>
2024-02-16 15:44:19 +0100

    added unit test for #381

--- tests/testthat/test-heat_plot.R
+++ tests/testthat/test-heat_plot.R
@@ -2,9 +2,18 @@ test_that("heat_plot works", {
   set.seed(1)
   model_fit <- compute_mallows(
     setup_rank_data(potato_visual),
-    compute_options = set_compute_options(nmc = 2)
+    compute_options = set_compute_options(nmc = 20, burnin = 3)
   )
   expect_s3_class(heat_plot(model_fit, burnin = 0, type = "MAP"), "ggplot")
+  obj <- heat_plot(model_fit)
+  expect_equal(sum(obj$data$probability), 20)
+
+  model_fit <- compute_mallows(
+    setup_rank_data(potato_visual),
+    compute_options = set_compute_options(nmc = 20, burnin = 3, rho_thinning = 3)
+  )
+  obj <- heat_plot(model_fit)
+  expect_equal(sum(obj$data$probability), 20)
 
   set.seed(1)
   model_fit <- compute_mallows(
