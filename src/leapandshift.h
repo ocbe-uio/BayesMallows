e1a311ff0062e784170ae40ea1630ec9f1887317
Øystein Sørensen <oystein_sorensen@hotmail.com>
2024-02-19 21:33:11 +0100

    renamed back

--- src/leapandshift.h
+++ src/leapandshift.h
@@ -4,22 +4,17 @@
 #include "distances.h"
 
 struct LeapShiftObject{
-  LeapShiftObject(const arma::vec& rho_proposal,
-                  const arma::uvec& indices = arma::uvec{},
-                  double prob_backward = 1, double prob_forward = 1) :
-  rho_proposal { rho_proposal },
-  indices { indices },
-  prob_backward { prob_backward },
-  prob_forward { prob_forward }{};
+  LeapShiftObject(const arma::vec& rho_proposal) :
+  rho_proposal { rho_proposal } {};
 
   ~LeapShiftObject() = default;
 
   void shift_step(const arma::vec& rho, int u);
 
   arma::vec rho_proposal;
-  arma::uvec indices;
-  double prob_backward;
-  double prob_forward;
+  arma::uvec indices{};
+  double prob_backward{};
+  double prob_forward{};
 };
 
 LeapShiftObject leap_and_shift(
