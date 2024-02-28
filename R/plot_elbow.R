f19be0f472b8217dfc07eba21cf205a7c9def585
Øystein Sørensen <oystein_sorensen@hotmail.com>
2024-02-28 14:18:58 +0100

    small improvement of docs to plot_elbow

--- R/plot_elbow.R
+++ R/plot_elbow.R
@@ -4,23 +4,22 @@
 #' consensus for different number of clusters. This function is useful for
 #' selecting the number of mixture.
 #'
-#' @param ... One or more objects returned from [compute_mallows()],
-#' separated by comma, or a list of such objects. Typically, each object
-#' has been run with a different number of mixtures, as specified in the
-#' `n_clusters` argument to [compute_mallows()].
+#' @param ... One or more objects returned from [compute_mallows()], separated
+#'   by comma, or a list of such objects. Typically, each object has been run
+#'   with a different number of mixtures, as specified in the `n_clusters`
+#'   argument to [compute_mallows()]. Alternatively an object returned from
+#'   [compute_mallows_mixtures()].
 #'
 #' @return A boxplot with the number of clusters on the horizontal axis and the
-#' with-cluster sum of distances on the vertical axis.
+#'   with-cluster sum of distances on the vertical axis.
 #'
 #' @export
 #'
 #' @example /inst/examples/compute_mallows_mixtures_example.R
 #' @family posterior quantities
 plot_elbow <- function(...) {
-  # Put the models into a list. These are typically fitted with different number of clusters
   models <- list(...)
 
-  # Taking into account the case where the user has entered a list of models
   if (length(models) == 1 && !(inherits(models[[1]], "BayesMallows"))) {
     models <- models[[1]]
   }
@@ -32,7 +31,8 @@ plot_elbow <- function(...) {
       stop("Please specify burnin with 'burnin(model_fit) <- value'.")
     }
 
-    if (length(unique(x$within_cluster_distance$iteration)) != x$compute_options$nmc) {
+    if (length(unique(x$within_cluster_distance$iteration))
+    != x$compute_options$nmc) {
       stop("To get an elbow plot, set include_wcd=TRUE in compute_mallows")
     }
 
@@ -42,13 +42,18 @@ plot_elbow <- function(...) {
     ]
 
     # Need to sum the within-cluster distances across clusters, for each iteration
-    df <- aggregate(x = list(value = df$value), by = list(iteration = df$iteration), FUN = sum)
+    df <- aggregate(
+      x = list(value = df$value),
+      by = list(iteration = df$iteration), FUN = sum
+    )
 
     df$n_clusters <- x$n_clusters
     return(df)
   }))
 
-  ggplot2::ggplot(df, ggplot2::aes(x = as.factor(.data$n_clusters), y = .data$value)) +
+  ggplot2::ggplot(
+    df, ggplot2::aes(x = as.factor(.data$n_clusters), y = .data$value)
+  ) +
     ggplot2::geom_boxplot() +
     ggplot2::xlab("Number of clusters") +
     ggplot2::ylab("Within-cluster sum of distances")
