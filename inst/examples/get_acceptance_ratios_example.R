190d85d1b74d4b8da36cc1bd4db552355430aee5
Øystein Sørensen <oystein_sorensen@hotmail.com>
2024-02-28 11:29:01 +0100

    updated example

--- inst/examples/get_acceptance_ratios_example.R
+++ inst/examples/get_acceptance_ratios_example.R
@@ -1,12 +1,7 @@
-library(parallel)
-cl <- makeCluster(2)
 set.seed(1)
 mod <- compute_mallows(
   data = setup_rank_data(potato_visual),
-  compute_options = set_compute_options(burnin = 200),
-  cl = cl
+  compute_options = set_compute_options(burnin = 200)
 )
-stopCluster(cl)
 
-# One ratio is returned for each cluster
 get_acceptance_ratios(mod)
