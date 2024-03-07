ae1a7ab3df20290342d20f47331c0091bcfce857
Øystein Sørensen <oystein_sorensen@hotmail.com>
2024-03-07 10:45:05 +0100

    styling

--- tests/testthat/test-smc_pairwise.R
+++ tests/testthat/test-smc_pairwise.R
@@ -3,17 +3,19 @@ test_that("update_mallows works with pairwise preferences", {
   dat <- subset(beach_preferences, assessor <= 10)
   mod <- compute_mallows(
     data = setup_rank_data(
-      preferences = beach_preferences),
+      preferences = beach_preferences
+    ),
     compute_options = set_compute_options(nmc = 3000, burnin = 1000)
   )
 
   # Next we provide assessors 21 to 60 one at a time.
-  for(i in 21:22){
+  for (i in 21:22) {
     mod <- update_mallows(
       model = mod,
       new_data = setup_rank_data(
         preferences = subset(beach_preferences, assessor == i),
-        user_ids = i, shuffle_unranked = TRUE),
+        user_ids = i, shuffle_unranked = TRUE
+      ),
       smc_options = set_smc_options(latent_sampling_lag = 0)
     )
   }
